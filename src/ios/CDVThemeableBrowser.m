8e59cdc292cce948afc23110d532917c17784f54
mcookman <mcookman@expansionhealth.com>
2016-09-02 10:45:23 -0400

    Add files via upload

--- src/ios/CDVThemeableBrowser.m
+++ src/ios/CDVThemeableBrowser.m
@@ -237,7 +237,7 @@
     }
 
     if (self.themeableBrowserViewController == nil) {
-        NSString* originalUA = [CDVUserAgentUtil originalUserAgent];
+        NSString* originalUA = @"Mozilla/5.0";//[CDVUserAgentUtil originalUserAgent];
         self.themeableBrowserViewController = [[CDVThemeableBrowserViewController alloc]
                                                initWithUserAgent:originalUA prevUserAgent:[self.commandDelegate userAgent]
                                                browserOptions: browserOptions
